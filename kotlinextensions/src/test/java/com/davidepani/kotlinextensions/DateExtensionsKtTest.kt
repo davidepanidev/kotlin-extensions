2a87cebda5ec7a9aba58189781969edcc7735a98
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-25 09:40:31 +0100

    [kotlinextensions] Added DateExtensions tests

--- kotlinextensions/src/test/java/com/davidepani/kotlinextensions/DateExtensionsKtTest.kt
+++ kotlinextensions/src/test/java/com/davidepani/kotlinextensions/DateExtensionsKtTest.kt
@@ -1,10 +1,27 @@
 package com.davidepani.kotlinextensions
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class DateExtensionsKtTest {
 
     @Test
-    fun toFormattedString() {
+    fun `toFormattedString yyyy-MM-dd format`() {
+        val date = Date(1648197341133)
+        val actualFormattedDate = date.toFormattedString("yyyy-MM-dd")
+        val expectedFormattedDate = "2022-03-25"
+
+        assertEquals(expectedFormattedDate, actualFormattedDate)
     }
+
+    @Test
+    fun `toFormattedString dd MM yyyy format`() {
+        val date = Date(1648197341133)
+        val actualFormattedDate = date.toFormattedString("dd/MM/yyyy")
+        val expectedFormattedDate = "25/03/2022"
+
+        assertEquals(expectedFormattedDate, actualFormattedDate)
+    }
+
 }
