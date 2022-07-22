c49ddbd32223508557e4a1d13adbdd5524e8587f
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-07-22 18:05:09 +0200

    [lib] DateTime extensions and tests

--- kotlinextensions/src/test/java/com/davidepani/kotlinextensions/DateExtensionsTest.kt
+++ kotlinextensions/src/test/java/com/davidepani/kotlinextensions/DateExtensionsTest.kt
@@ -2,6 +2,12 @@ package com.davidepani.kotlinextensions
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 class DateExtensionsTest {
@@ -22,6 +28,81 @@ class DateExtensionsTest {
         val expectedFormattedDate = "25/03/2022"
 
         assertEquals(expectedFormattedDate, actualFormattedDate)
+    }
+
+
+    @Test
+    fun `toFormattedString returns localized SHORT formatted String`() {
+        val date = LocalDateTime.of(2022, 12, 12, 12, 12)
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).withLocale(Locale.ITALIAN)
+        val actualFormattedDate = date.toFormattedString(formatter)
+        val expectedFormattedDate = "12/12/22, 12:12"
+
+        assertEquals(expectedFormattedDate, actualFormattedDate)
+    }
+
+    @Test
+    fun `toFormattedDateWithTimeString returns localized SHORT formatted String`() {
+        val date = LocalDateTime.of(2022, 12, 12, 12, 12)
+        val actualFormattedDate = date.toFormattedDateWithTimeString(
+            formatStyle = FormatStyle.SHORT,
+            locale = Locale.ITALIAN
+        )
+        val expectedFormattedDate = "12/12/22, 12:12"
+
+        assertEquals(expectedFormattedDate, actualFormattedDate)
+    }
+
+    @Test
+    fun `toFormattedDateString returns localized SHORT formatted String`() {
+        val date = LocalDateTime.of(2022, 12, 12, 12, 12)
+        val actualFormattedDate = date.toFormattedDateString(
+            formatStyle = FormatStyle.SHORT,
+            locale = Locale.ITALIAN
+        )
+        val expectedFormattedDate = "12/12/22"
+
+        assertEquals(expectedFormattedDate, actualFormattedDate)
+    }
+
+    @Test
+    fun `toFormattedDateOnlyString returns localized SHORT formatted String`() {
+        val date = LocalDateTime.of(2022, 12, 12, 12, 12)
+        val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(Locale.ITALIAN)
+        val actualFormattedDate = date.toFormattedDateOnlyString(formatter)
+        val expectedFormattedDate = "12/12/22"
+
+        assertEquals(expectedFormattedDate, actualFormattedDate)
+    }
+
+    @Test
+    fun `minutesBetween returns correct minutes difference`() {
+        val date = LocalDateTime.of(2022, 12, 12, 12, 12)
+        val date2 = LocalDateTime.of(2022, 12, 12, 12, 9)
+        val expectedDifferenceValue = -3L
+        val actualDifferenceValue = date.minutesBetween(date2)
+
+        expectThat(actualDifferenceValue).isEqualTo(expectedDifferenceValue)
+    }
+
+    @Test
+    fun `differenceBetween returns correct days difference`() {
+        val date = LocalDateTime.of(2022, 12, 12, 12, 12)
+        val date2 = LocalDateTime.of(2022, 12, 12, 12, 9)
+        val expectedDifferenceValue = 0L
+        val actualDifferenceValue = date.differenceBetween(date2, ChronoUnit.DAYS)
+
+        expectThat(actualDifferenceValue).isEqualTo(expectedDifferenceValue)
+    }
+
+    @Test
+    fun `differenceBetween returns correct years difference`() {
+        val date = LocalDateTime.of(2021, 12, 12, 12, 9)
+        val date2 = LocalDateTime.of(2022, 12, 12, 12, 9)
+        val expectedDifferenceValue = 1L
+        val actualDifferenceValue = date.differenceBetween(date2, ChronoUnit.YEARS)
+
+        expectThat(actualDifferenceValue).isEqualTo(expectedDifferenceValue)
     }
 
 }
