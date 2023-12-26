b260a34f43812d2195c83fe1945cf4c89ba2b180
davidepanidev <102189224+davidepanidev@users.noreply.github.com>
2023-12-26 12:40:12 +0100

    added methods to NumberExtensions

--- kotlinextensions/src/main/java/com/github/davidepanidev/kotlinextensions/NumberExtensions.kt
+++ kotlinextensions/src/main/java/com/github/davidepanidev/kotlinextensions/NumberExtensions.kt
@@ -2,6 +2,9 @@ package com.github.davidepanidev.kotlinextensions
 
 import com.github.davidepanidev.kotlinextensions.utils.currencyformatter.CurrencyFormatter
 import com.github.davidepanidev.kotlinextensions.utils.numberformatter.NumberFormatter
+import java.math.RoundingMode
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 
 fun Number.formatToCurrency(currencyFormatter: CurrencyFormatter): String {
@@ -19,4 +22,12 @@ fun Number.formatToCurrency(currencyFormatter: CurrencyFormatter, customCurrency
 
 fun Number.formatToPercentage(numberFormatter: NumberFormatter): String {
     return numberFormatter.formatToPercentage(amount = this)
+}
+
+fun Long.toLocalDateTime(offset: ZoneOffset = ZoneOffset.UTC): LocalDateTime {
+    return LocalDateTime.ofEpochSecond(this, 0, offset)
+}
+
+fun Double.roundToNDecimals(decimals: Int, roundingMode: RoundingMode = RoundingMode.UP): Double {
+    return this.toBigDecimal().setScale(decimals, roundingMode).toDouble()
 }
